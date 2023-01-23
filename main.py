9dc85597c0c18344528eb36abe21195ee536ed7c
Superhackah204 <jeddrasco123@gmail.com>
2023-01-23 19:55:41 +0800

    main.py: File Size Display

--- main.py
+++ main.py
@@ -19,6 +19,28 @@ def compress_file(filenm):
     with zipfile.ZipFile('test.zip', 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip:
         # Compress based on the file name provided
         zip.write(filenm)
+
+    # display original file size definition/function
+    def display_filesize(filenm):
+        ogfilesize = (os.path.getsize(filenm)) / 1024
+        ogfilesize = round(ogfilesize, 2)
+        ogfile = str(ogfilesize)
+
+        return ogfile
+
+    # display compressed file size definition/function
+    def display_compressedfilesize(compressed_filenm):
+        compfilesize = (os.path.getsize(compressed_filenm)) / 1024
+        compfilesize = round(compfilesize, 2)
+        compfile = str(compfilesize)
+
+        return compfile
+
+    # ogfile = display_filesize(filenm)
+    # compfile = display_compressedfilesize(compressed_filenm)
+
+    # label_sz.configure(text="Original file size is " + ogfile + " kb" + "\n\n Compressed file size is ")
+
 # decompress_file definition/function
 def decompress_file(compressed_filenm):
     
@@ -27,22 +49,16 @@ def decompress_file(compressed_filenm):
         # Decompress the file name provided to a folder named "extracted"
         zip.extractall(path="extracted")
 
-# display original file size definition/function
-def display_filesize(filenm):
-    ogfilesize = (os.path.getsize(filenm))/1024
-    print(f'{round(ogfilesize,2)} kb')
-
-# display compressed file size definition/function
-def display_compressedfilesize(compressed_filenm):
-    compfilesize = (os.path.getsize(compressed_filenm)/1024
-    print(f'{round(compfilesize,2)} kb')
-
 window = gui.Tk()
 window.title(prog_nm+"-("+prog_ver+")")
 header = gui.Label(window, text=prog_nm+"-"+prog_ver, height=5, font="Helvetica 20 bold")
 header.pack()
 button_extract = gui.Button(window, text = "Compress", command = lambda: compress_file("testfile.txt"))
 button_extract.pack()
+
+label_sz = gui.Label(window, text = "", width = 100, height = 4, fg = "blue")
+label_sz.pack()
+
 exit_button = gui.Button(window, text='Exit', command=window.destroy)
 exit_button.pack()
 window.mainloop()
